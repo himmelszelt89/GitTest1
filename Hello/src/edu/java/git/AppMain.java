c05ce78a88dc5dd943ed869ae99261040b68f4da
himmelszelt89 <ep249819@empas.com>
2016-11-11 15:13:08 +0900

    Update AppMain.java

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -9,7 +9,7 @@ public class AppMain {
 		System.out.println("[수정1] 안녕하세요!");
         System.out.println("[modify2] Web_Git modify");
         
-        System.out.println("새로운 기능 추가할꺼예요~~");
+        System.out.println("bug fix");
 		
 	} // end main()
 
