eebf84a97c93800a2bb2a55bab5e4bf4227e779f
himmelszelt89 <ep249819@empas.com>
2016-11-11 14:50:00 +0900

    multi_modify
    multi_modify_ test

--- Hello/src/edu/java/git/AppMain.java
+++ Hello/src/edu/java/git/AppMain.java
@@ -5,7 +5,7 @@ public class AppMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Hello, Git!");
+		System.out.println("Hello, Git!!");
 		System.out.println("[수정1] 안녕하세요!");
         System.out.println("[modify2] Web_Git modify");
 		
