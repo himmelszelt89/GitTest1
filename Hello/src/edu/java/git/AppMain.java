a0dc1bb4b04ced39c2c3ba7599f0f450aa7b76a5
stu <stu@DESKTOP-T50JSKS>
2016-11-11 13:58:11 +0900

    첫번째 커밋

---
+++ Hello/src/edu/java/git/AppMain.java
@@ -0,0 +1,12 @@
+package edu.java.git;
+
+public class AppMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello, World!");
+		
+	} // end main()
+
+} // end class AppMain
